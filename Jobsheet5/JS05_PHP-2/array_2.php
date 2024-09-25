ba4b1487865ec5bc97370f705d49441f4becab71
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2024-09-25 13:46:53 +0700

    Js5-Praktikum 2

---
+++ Jobsheet5/JS05_PHP-2/array_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="array_2style.css">
+</head>
+<body style="font-family: Arial;" class="cihuy">
+    <?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'perempuan'
+    ];
+
+    echo "Nama : {$Dosen ['nama']} <br>";
+    echo "Domisili:  {$Dosen ['domisili']} <br>";
+    echo  "Jenis kelamin : {$Dosen ['jenis_kelamin']} <br>";
+    ?>
+</body>
+</html>
